ac15e438984b655748ad5e9aa1bcdf7519d24840
Marty Curry <martycurry7@gmail.com>
2021-05-15 15:53:24 -0400

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,21 @@
+#clear the terminal screen
+import os
+os.system('clear')
+
+
+#variables
+ # can change the variable later on like below
+ # always use smart variable names  
+ # use underscore 
+ # can use numbers, text, other variables, 
+
+
+full_name = "John Elder" 
+print (full_name)
+
+first_name = "Marty"  
+print (first_name)
+
+first_name = ""  
+print (first_name)
+
